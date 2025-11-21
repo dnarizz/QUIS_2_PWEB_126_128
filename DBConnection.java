8925666ada400eddf855b9360b4b0ecc676a4947
dnarizz <danarrizkya2606@gmail.com>
2025-11-21 21:16:40 +0700

    Refactor database

--- DBConnection.java
+++ DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/library_website";
-    private static final String USER = "root"; // Ganti dengan user DB kamu
-    private static final String PASSWORD = ""; // Ganti dengan password DB kamu
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() {
         Connection connection = null;
